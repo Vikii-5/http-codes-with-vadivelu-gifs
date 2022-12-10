59486cabc312f337412f343f472077888b51d79e
Vikii-5 <72004386+Vikii-5@users.noreply.github.com>
2022-12-10 15:55:48 +0530

    image format changed to jpg

--- script.js
+++ script.js
@@ -6,18 +6,18 @@ container.appendChild(content);
 
 let heading = document.createElement('h3');
 heading.className = 'heading text-center';
-heading.innerHTML = "HTTP Status Codes with Vadivelu Gif's"
+heading.innerHTML = "HTTP Status Codes with Vadivelu JPG's"
 content.appendChild(heading);
 
 let subHeading = document.createElement('h5');
-subHeading.innerText = "Save Gif's and use it your Projects"
+subHeading.innerText = "Save JPG's and use it your Projects"
 content.appendChild(subHeading);
 
 let selectCode = document.createElement('select');
 selectCode.className = 'form-select';
 content.appendChild(selectCode);
 
-let statusCodes = ['100 - Continue', '101 - Switching Protocols', '200 - OK', '201 - Accepted', '206 - Partial Content', '301 - Moved Permanently', '302 - Found', '307 - Temporary Redirect', '400 - Bad Request', '401 - Unauthorized', '402 - Payment Required', '403 - Forbidden', '404 - Not Found', '405 - Method Not Allowed', '406 - Not Acceptable', '408 - Request Timeout', '409 - Conflict', '410 - Gone', '411 - Length Required', '412 - Precondition Failed', '417 - Expectation Failed', '421 - Misdirect Request', '422 - Unprocessable Entity', '423 - Locked', '424 - Failed Dependency', '499 - Client Closed Request', '500 - Internal Server Error', '502 - Bad Gateway', '506 - Variant also Negotiates', '508 - Loop Detected'];
+let statusCodes = ['100 - Continue', '101 - Switching Protocols', '200 - OK', '201 - Accepted', '206 - Partial Content', '301 - Moved Permanently', '302 - Found', '307 - Temporary Redirect', '400 - Bad Request', '401 - Unauthorized', '402 - Payment Required', '403 - Forbidden', '404 - Not Found', '405 - Method Not Allowed', '406 - Not Acceptable', '408 - Request Timeout', '409 - Conflict', '410 - Gone', '411 - Length Required', '412 - Precondition Failed', '417 - Expectation Failed', '422 - Unprocessable Entity', '423 - Locked', '500 - Internal Server Error', '502 - Bad Gateway'];
 
 statusCodes.forEach((code) => {
     let option = document.createElement('option');
@@ -28,20 +28,20 @@ statusCodes.forEach((code) => {
 
 let button = document.createElement('button');
 button.type = 'submit';
-button.innerText = 'Get Thalaivan Gif'
+button.innerText = 'Get Thalaivan JPG'
 button.className = 'btn btn-outline-light';
 content.appendChild(button);
 
-let gif = document.createElement('img');
-content.appendChild(gif);
+let jpg = document.createElement('img');
+content.appendChild(jpg);
 
 button.addEventListener('click', () => {
 
     if(true){
-        gif.classList.add('hidden');
+        jpg.classList.add('hidden');
     }
 
     let url = `https://vadivelu.anoram.com/jpg/${selectCode.value}`;
-    gif.classList.remove('hidden');
-    gif.src = url;
+    jpg.classList.remove('hidden');
+    jpg.src = url;
 })
